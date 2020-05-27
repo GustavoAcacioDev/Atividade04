5294372a63de37d740b7f768b96df8cf3a3bd786
unknown <gustavoacacio22@gmail.com>
2020-05-27 10:50:25 -0300

    Atividade finalizada

--- Celular.cs
+++ Celular.cs
@@ -2,6 +2,71 @@ namespace Atividade04
 {
     public class Celular
     {
+        public string cor;
+        public string modelo;
+        public float tamanho;
+        public bool retorno;
+        public string celularLigando;
+        public string celularDesligando;
+        public string ligacao;
+        public string mensagem;
+        public string seLigar;
         
+
+
+
+        /// <summary>
+        /// Melhora a resposta do usuário
+        /// </summary>
+        /// <param name="condicao">texto capturado pelo console</param>
+        /// <returns>Se o celular quer ser ligado ou não</returns>
+        public bool TraduzirConsole(string condicao){
+            if(condicao == "sim" || condicao == "Sim" || condicao == "sIm" ){
+                retorno = true;
+            }else{
+                retorno = false; 
+            }
+            return retorno;
+        }
+
+        /// <summary>
+        /// Ligar o celular
+        /// </summary>
+        /// <returns>Se foi ligado o celular ou não</returns>
+        public string LigandoCelular(){
+            if(retorno == true){
+                celularLigando = "O celular foi ligado";
+            }
+            return celularLigando;
+        }
+
+        public string FazendoLigacao(){
+            if(retorno == true){
+                ligacao = "Realizando ligação";
+            }else{
+                ligacao = "Não foi possível realizar a ligação";
+            }
+            return ligacao;
+        }
+
+        public string MandandoMensagem(){
+            
+            if(retorno == true){
+                mensagem = "Digite sua mensagem:";
+            }else{
+                mensagem = "Não foi possível mandar a sua mensagem";
+            }
+            return mensagem;
+        }
+
+        public string Desligando(){
+            if(retorno == true){
+                celularDesligando = "O celular foi desligado";
+            }else{
+                
+            }
+            return celularDesligando;
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,42 @@ namespace Atividade04
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Celular galaxy = new Celular();
+            galaxy.modelo = "Galaxy S9";
+            galaxy.cor = "Branco";
+            galaxy.tamanho = 14.7f;
+
+
+            Console.WriteLine("Você deseja ligar o celular?");
+            galaxy.retorno = galaxy.TraduzirConsole( Console.ReadLine() );
+            Console.WriteLine(galaxy.LigandoCelular());
+            if(galaxy.retorno == true){
+
+
+                Console.WriteLine("Você deseja fazer uma ligação?");
+                galaxy.retorno = galaxy.TraduzirConsole( Console.ReadLine() );
+                Console.WriteLine(galaxy.FazendoLigacao());
+
+
+                Console.WriteLine("Você deseja enviar uma mensagem?");
+                galaxy.retorno = galaxy.TraduzirConsole( Console.ReadLine() );
+            
+                if(galaxy.retorno){
+
+                    Console.WriteLine(galaxy.MandandoMensagem());
+                    string mensagemArmazenada = Console.ReadLine();
+                    Console.WriteLine("Você enviou a seguinte mensagem: "+mensagemArmazenada);
+                }else{
+                    Console.WriteLine("Você não enviou nenhuma mensagem");
+                }
+
+                Console.WriteLine("Você deseja desligar o celular?");
+                galaxy.retorno = galaxy.TraduzirConsole( Console.ReadLine() );
+                Console.WriteLine(galaxy.Desligando());
+            
+            }else{
+                Console.WriteLine("O celular está desligado");
+            }
         }
     }
 }
